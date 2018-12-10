8f77efaeb5bbd67cbd858fcafaae2e1660bdf64b
bus7d <bus7dz@gmail.com>
2018-12-10 21:25:22 +0100

    Create alphanumzip.py

---
+++ alphanumzip.py
@@ -0,0 +1,18 @@
+	#!/usr/bin/python
+	#passw="v"
+	#passw="p"
+	#passw="r"
+	passw="y"
+	x=0
+	alphanum="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	alphanum.split()
+	#wordz="vlvffw"
+	#wordz="ywgCgyf'gC"
+	#wordz="vdddueDw"
+	wordz="fb"
+	while x<len(alphanum):
+				password=passw+wordz+alphanum[x]
+				x=x+1
+				print password
+
+
